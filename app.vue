b5d06f2d38fa40b847b6fd08d5c4e5044c37d803
riverahugo <rrv2010hugo@hotmail.com>
2025-07-10 20:02:24 -0600

    Nuxt page modifiacion

--- app.vue
+++ app.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-import Index from './pages/index.vue';
-
-</script>
 
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <Index/>
+    <NuxtPage/>
   </div>
 </template>
