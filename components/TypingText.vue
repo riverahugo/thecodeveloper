20e5c64ed11a9bfbc7fef873d5b6e55da24570cd
riverahugo <rrv2010hugo@hotmail.com>
2025-07-10 19:12:31 -0600

    Subiendo en github

---
+++ components/TypingText.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1 ref="el" class="typing"></h1>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import Typed from 'typed.js'
+
+const el = ref(null)
+
+onMounted(() => {
+  const typed = new Typed(el.value, {
+    strings: [
+      '🚀 Proximamente...',
+      '🌐 ¡The Codeveloper!',
+    ],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 2000,
+    loop: true,
+    smartBackspace: true,
+    showCursor: true,
+    cursorChar: '|',
+  })
+})
+</script>
+
+<style scoped>
+.typing {
+  color: #2e7d32;
+  font-size: 1.8rem;
+  text-align: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import TypingText from '~/components/TypingText.vue';
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="content">
+      <TypingText/>
+      <p>¡Hola desde index.vue!</p>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(135deg, #a8e6cf, #dcedc1);
+  padding: 20px;
+}
+
+.content {
+  text-align: center;
+  max-width: 90%;
+  background: rgba(255, 255, 255, 0.15);
+  padding: 2rem;
+  border-radius: 12px;
+  backdrop-filter: blur(4px);
+  color: #2e7d32;
+  font-family: 'Segoe UI', sans-serif;
+}
+</style>
