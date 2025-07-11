3c7b7cafd11940801feb9890e706ea5c244dc425
riverahugo <rrv2010hugo@hotmail.com>
2025-07-10 20:22:57 -0600

    Nitro preset static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  ssr:false
+  ssr:false,
+    nitro: {
+    preset: 'static' // para que genere archivos estáticos
+  }
 })
