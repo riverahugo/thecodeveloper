ee90243fadd254af79470c09e2668401d833dc79
riverahugo <rrv2010hugo@hotmail.com>
2025-12-22 22:04:32 -0600

    Configure custom domain thecodeveloper.com

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,12 @@
-  // https://nuxt.com/docs/api/configuration/nuxt-config
-  export default defineNuxtConfig({
-      app: {
-    baseURL: '/thecodeveloper/',
-      buildAssetsDir: '/_nuxt/'
-    },
-    compatibilityDate: '2025-05-15',
-    devtools: { enabled: true },
-    ssr:false,
-      nitro: {
-      preset: 'static' // para que genere archivos estáticos
-    }
-  })
+ export default defineNuxtConfig({
+  app: {
+    baseURL: '/', // ← Cambiar esto
+    buildAssetsDir: '/_nuxt/'
+  },
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  ssr: false,
+  nitro: {
+    preset: 'static'
+  }
+})
