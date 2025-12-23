1ba8697760e8cd35ffe6bd9a36a7b1d673bf6739
riverahugo <rrv2010hugo@hotmail.com>
2025-12-22 20:35:01 -0600

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
-   baseURL: '/thecodeveloper/'
+   baseURL: '/thecodeveloper/',
+    buildAssetsDir: '/_nuxt/'
   },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
