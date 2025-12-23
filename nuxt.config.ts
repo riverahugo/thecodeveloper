05861502c3495d022c2dc667867b2af80edc2719
riverahugo <rrv2010hugo@hotmail.com>
2025-12-22 21:33:05 -0600

    fix nuxt assets path for github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
-   baseURL: '/thecodeveloper/',
-    buildAssetsDir: '_nuxt/'
+  baseURL: '/thecodeveloper/',
+    buildAssetsDir: '/_nuxt/'
   },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
